e252fd0061c5f28132dbb4f129a744ac85c29ff2
Carlos Castro <alu0100819847@ull.edu.es>
2018-05-08 09:24:14 +0100

    Correcciones

---
+++ cliente/js/ubicacion.js
@@ -0,0 +1,15 @@
+function myMap() {
+  var myCenter = new google.maps.LatLng(28.3580804,-16.384250);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+
+  // Zoom to 9 when clicking on marker
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(9);
+    map.setCenter(marker.getPosition());
+  });
+}
